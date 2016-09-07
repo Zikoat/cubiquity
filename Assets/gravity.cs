2d41e92393ef5d7df2f73307a82958531c841eaf
sigurd-laptop <sschoeler99@gmail.com>
2016-09-07 14:11:20 +0200

    moved gravity to player script and tried to make player point away from camera, but this broke things

--- Assets/gravity.cs
+++ Assets/gravity.cs
@@ -10,12 +10,7 @@ public class gravity : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		RaycastHit hit;
-		Ray ray = new Ray(transform.position, -transform.up);
-		if (Physics.Raycast (ray, out hit)) {
-			Physics.gravity = -hit.normal.normalized * 10f;
-		}
-		transform.up = hit.normal;
-		Debug.DrawRay (transform.position, transform.up, Color.blue, 0, false);
-	}
+
+
+    }
 }
--- Assets/player.cs
+++ Assets/player.cs
@@ -24,23 +24,46 @@ public class player : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		
+
+        // declarations
+        Transform camera = Camera.main.transform;
 		Rigidbody rb = GetComponent<Rigidbody> ();
 
-		//rb.AddRelativeForce(Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"), ForceMode.VelocityChange);
+        // find up
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Physics.gravity = -hit.normal.normalized * 10f;
+        }
+        // transform.up = hit.normal; // done with lookuprotation
+        Debug.DrawRay(transform.position, transform.up, Color.blue, 0, false);
 
-		Vector3 vel = rb.velocity; // first we get the velocity // transform.InverseTransformDirection (rb.velocity);
 
+        // find forward
+        Vector3 forward = (camera.position - transform.position);
+        forward = transform.InverseTransformDirection(forward);
+        forward.y = 0;
+        forward = transform.TransformDirection(forward);
+        // transform.forward = forward;
+        Debug.DrawRay(transform.position, forward, Color.yellow);
+
+        Quaternion rotation = new Quaternion();
+        rotation.SetLookRotation(forward, hit.normal);
+        //transform.rotation.SetLookRotation(forward, hit.normal);
+        transform.rotation = rotation;
+        
+
+        // change velocity
+        Vector3 vel = rb.velocity; // first we get the velocity
 		vel = transform.InverseTransformDirection (vel); // velocity is in world space, transform to local space
-		// change the velocity
 		vel.x = Input.GetAxis ("Horizontal") * speed;
-		vel.z = Input.GetAxis ("Vertical") * speed; // Mathf.Clamp(vel.y, -5, 5)
-
+		vel.z = Input.GetAxis ("Vertical") * speed;
 		vel = transform.TransformDirection (vel); // change vel back to world space
-
-		rb.velocity = vel;
+        rb.velocity = vel;
 		Debug.DrawRay(transform.position, vel);
-		//transform.LookAt (Camera.main.transform);
 
-	}
+
+
+    }
 }
