fdf8990b5a11ca242a64d060dcaacf7f5cf9f245
Sigurd stasjonær <sschoeler99@gmail.com>
2016-09-10 19:19:55 +0200

    ordered player.cs

--- Assets/player.cs
+++ Assets/player.cs
@@ -12,15 +12,6 @@ public class player : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if (Physics.Raycast (ray, out hit)) {
-			Debug.DrawRay (hit.point, hit.normal * 2, Color.magenta);
-			if (Input.GetMouseButton (0)) {
-				Debug.Log (hit.normal);
-			}
-		}
-		Debug.DrawRay (transform.position, transform.forward, Color.blue, 0, false);
 	}
 
 	void FixedUpdate() {
@@ -29,30 +20,21 @@ public class player : MonoBehaviour {
         Transform camera = Camera.main.transform;
 		Rigidbody rb = GetComponent<Rigidbody> ();
 
-        // find up
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position, -transform.up);
-        if (Physics.Raycast(ray, out hit))
-        {
-            Physics.gravity = -hit.normal.normalized * 10f;
-        }
-        // transform.up = hit.normal; // done with lookuprotation
-        Debug.DrawRay(transform.position, transform.up, Color.blue, 0, false);
-
+        
 
         // find forward
-        Vector3 forward = (camera.position - transform.position);
-        forward = transform.InverseTransformDirection(forward);
+        Vector3 forward = (transform.position - camera.position);
+        Debug.DrawRay(transform.position, forward, Color.yellow);
+        Debug.DrawRay(transform.position, transform.localEulerAngles, Color.yellow);
+        /*forward = transform.InverseTransformDirection(forward);
         forward.y = 0;
         forward = transform.TransformDirection(forward);
         // transform.forward = forward;
-        Debug.DrawRay(transform.position, forward, Color.yellow);
 
-        Quaternion rotation = new Quaternion();
-        rotation.SetLookRotation(forward, hit.normal);
-        //transform.rotation.SetLookRotation(forward, hit.normal);
-        transform.rotation = rotation;
-        
+        // Quaternion rotation = new Quaternion();
+        // rotation.SetLookRotation(forward, hit.normal);
+        // transform.localRotation = rotation;*/
+
 
         // change velocity
         Vector3 vel = rb.velocity; // first we get the velocity
@@ -62,6 +44,19 @@ public class player : MonoBehaviour {
 		vel = transform.TransformDirection (vel); // change vel back to world space
         rb.velocity = vel;
 		Debug.DrawRay(transform.position, vel);
+
+        // find up
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, -transform.up);
+        Debug.DrawRay(transform.position, -transform.up);
+        if (Physics.Raycast(ray, out hit))
+        {
+            Physics.gravity = -hit.normal.normalized * 10f;
+        }
+        transform.up = hit.normal; 
+
+        Debug.DrawRay(transform.position, -transform.up, Color.blue, 0, false);
+        Debug.DrawRay(transform.position, transform.forward, Color.blue, 0, false);
 
 
 
