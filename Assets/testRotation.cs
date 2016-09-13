ec995f552d9b6746411e6773ec060848cc9e10c4
sigurd-laptop <sschoeler99@gmail.com>
2016-09-13 23:27:25 +0200

    glorious, actually working testRotatin.cs script. haven't tried if actually works, and i need to add the calculated direction for local up. also made a level to test, could be deleted

---
+++ Assets/testRotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 relativePos = Camera.main.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        rotation.eulerAngles = new Vector3(0, rotation.eulerAngles.y, 0);
+
+        transform.rotation = rotation;
+
+        Debug.DrawRay(transform.position, transform.forward, Color.blue, 0, false);
+        Debug.DrawRay(transform.position, -transform.up, Color.blue, 0, false);
+
+
+    }
+}
